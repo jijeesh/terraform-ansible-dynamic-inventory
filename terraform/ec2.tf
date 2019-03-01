de996539f61ddf34844761dba551de9d876cf98b
Jijeesh <jijeesh.ka@gmail.com>
2019-03-01 14:17:41 +0530

    ansible and crontab testing

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,8 +1,19 @@
 locals {
   # The default username for our AMI
-  vm_user = "ubuntu"
+  vm_user = "ec2-user"
 }
 
+data "aws_ami" "amazon_linux" {
+ most_recent = true
+ filter {
+   name = "owner-alias"
+   values = ["amazon"]
+ }
+ filter {
+   name = "name"
+   values = ["amzn-ami-hvm-*-x86_64-gp2"]
+ }
+}
 # Get the latest Ubuntu Xenial AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -28,7 +39,7 @@ resource "aws_key_pair" "auth" {
 resource "aws_instance" "web" {
   instance_type = "t2.micro"
   tags = "${var.tags}"
-  ami = "${data.aws_ami.ubuntu.id}"
+  ami = "${data.aws_ami.amazon_linux.id}"
 
   # The name of our SSH keypair
   key_name = "${var.key_name}"
@@ -38,6 +49,32 @@ resource "aws_instance" "web" {
 
   subnet_id = "${aws_subnet.default.id}"
 
+provisioner "file" {
+    source      = "scripts/cleanup.sh"
+    destination = "~/cleanup.sh"
+    connection {
+      user = "${local.vm_user}"
+
+    }
+  }
+  provisioner "file" {
+      source      = "scripts/crontab"
+      destination = "~/crontab"
+      connection {
+        user = "${local.vm_user}"
+      }
+    }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mv ~/cleanup.sh /usr/local/src/cleanup.sh",
+      "sudo chmod +x /usr/local/src/cleanup.sh",
+      "echo '* * * * * /bin/nice -5 /usr/local/src/cleanup.sh' | sudo crontab -",
+      ]
+      connection {
+        user = "${local.vm_user}"
+      }
+  }
+  #
   # force Terraform to wait until a connection can be made, so that Ansible doesn't fail when trying to provision
   provisioner "remote-exec" {
     # The connection will use the local SSH agent for authentication
---
+++ terraform/scripts/cleanup.sh
@@ -0,0 +1,4 @@
+T=$(date +%Y%m%d-%H%M%S)
+L=/var/log/test-$(date +%Y%m%d-%H%M%S).log
+
+echo "$T> cleaning up " >> $L
